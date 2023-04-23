0df95742dd64d782147888622def0840a4e61438
Xiangning2020 <61442496+Xiangning2020@users.noreply.github.com>
2023-04-22 22:04:27 -0400

    normalize

--- data.py
+++ data.py
@@ -2,12 +2,23 @@ import pandas as pd
 from datetime import datetime
 from sklearn.neighbors import NearestNeighbors
 
-def merge_user_rows(df):
-   id_counts = dict(df['ID'].value_counts())
-   merged_df = df.groupby('ID').sum().reset_index()
-   merged_df['Duration (min)'] = merged_df.apply(lambda x: x['Duration (min)'] / id_counts[x['ID']], axis=1)
-   merged_df['Calories Consumed'] = merged_df.apply(lambda x: x['Calories Consumed'] / id_counts[x['ID']], axis=1)
-   return merged_df.astype(int)
+
+def merge_user_rows(user_df, duration_min, duration_max, calories_min,  calories_max):
+    id_counts = dict(user_df['ID'].value_counts())
+    merged_df = user_df.groupby('ID').sum().reset_index()
+    merged_df['Duration (min)'] = merged_df.apply(
+        lambda x: x['Duration (min)'] / id_counts[x['ID']], axis=1)
+    merged_df['Calories Consumed'] = merged_df.apply(
+        lambda x: x['Calories Consumed'] / id_counts[x['ID']], axis=1)
+    # print(f"duration_range: {duration_range}")
+    # print(f"calories_range: {calories_range}")
+    merged_df["Duration (min)"] = (merged_df["Duration (min)"] - duration_min
+                                   ) / (duration_max - duration_min)
+    merged_df["Calories Consumed"] = (merged_df["Calories Consumed"] - calories_min) / (
+        calories_max - calories_min)
+    print(duration_max - duration_min)
+    print(calories_max - calories_min)
+    return merged_df
 
 # def time_to_float(time_str):
 #       # parse time string into datetime object
@@ -19,53 +30,56 @@ def merge_user_rows(df):
 #       time_float = hour + minute / 60
 #       return time_float
 
+
 def handle_categorical(df):
-  # sport type
-  df = df.join(pd.get_dummies(df["Sport Type"]))
-  df = df.drop("Sport Type", axis=1)
-  # weekday
-  df = df.join(pd.get_dummies(df["Weekday"]))
-  df = df.drop("Weekday", axis=1)
-  # start time
-  # df["Start Time in float"] = df["Start Time"].apply(time_to_float)
-  # df = df.drop("Start Time", axis=1)
-  return df
-
-def generate_input(user_info):
-  df2 = pd.DataFrame(columns=df.columns)
-  row_num = len(user_info)
-  for row in user_info:
-    new_row = {
-              "ID": row[0],
-              row[1]: 1,
-              row[2]: 1,
-              "Duration (min)": row[3],
-              "Calories Consumed": row[4]}
-    df2 = df2.append(new_row, ignore_index=True)
-    df2 = df2.fillna(0)
-  return df2
+    # sport type
+    df = df.join(pd.get_dummies(df["Sport Type"]))
+    df = df.drop("Sport Type", axis=1)
+    # weekday
+    df = df.join(pd.get_dummies(df["Weekday"]))
+    df = df.drop("Weekday", axis=1)
+    # start time
+    # df["Start Time in float"] = df["Start Time"].apply(time_to_float)
+    # df = df.drop("Start Time", axis=1)
+    return df
 
 
-df = pd.read_csv("sport_data.csv")
-# drop location
-df = df.drop('Location (City)', axis=1)
-df = handle_categorical(df)
-df = merge_user_rows(df)
+def generate_input(user_info, df):
+    df2 = pd.DataFrame(columns=df.columns)
+    row_num = len(user_info)
+    for row in user_info:
+        new_row = {
+            "ID": row[0],
+            row[1]: 1,
+            row[2]: 1,
+            "Duration (min)": row[3],
+            "Calories Consumed": row[4]}
+        df2 = df2.append(new_row, ignore_index=True)
+        df2 = df2.fillna(0)
+    return df2
 
-# fit model
-X = df[list(df.columns)[1:]]
-neigh = NearestNeighbors(n_neighbors=2)
-nbrs = neigh.fit(X)
 
-query = [
-  [11, "Monday", "Basketball", 120, 800],
-  [11, "Sunday", "Running", 60, 400]
-]
-input_df = generate_input(query)
-processed_input = merge_user_rows(input_df).iloc[:,1:].values.tolist()
-print(processed_input)
+# df = pd.read_csv("sport_data.csv")
+# # drop location
+# df = df.drop('Location (City)', axis=1)
+# df = handle_categorical(df)
+# df = merge_user_rows(df)
 
-distances, indices = nbrs.kneighbors(processed_input)
-print(indices)
-for index in indices:
-  print(df.iloc[index])
+# # fit model
+# X = df[list(df.columns)]
+# neigh = NearestNeighbors(n_neighbors=2)
+# nbrs = neigh.fit(X)
+
+# query = [
+#     [11, "Monday", "Basketball", 120, 800],
+#     [11, "Sunday", "Running", 60, 400]
+# ]
+# input_df = generate_input(query)
+# processed_input = merge_user_rows(input_df).iloc[:, 1:].values.tolist()
+# print(processed_input)
+
+# distances, indices = nbrs.kneighbors(processed_input)
+# print(indices)
+# usernames = []
+# for index in indices[0]:
+#     print(df.iloc[index])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+from data import *
 
 import matplotlib.pyplot as plt
 from sklearn import datasets
@@ -41,9 +42,16 @@ sport_type = st.sidebar.selectbox(
 )
 
 duration = st.sidebar.selectbox(
-    'Select duration',
-    ('5min', '10min', '15min', '20min', '25min', '30min', '35mmin', '40min', '45min',
-     '50min', '55min', '60min', '65min', '70min', '75min', '80min', '85min', '90min')
+    'Select duration(min)',
+    (5, 10, 15, 20, 25, 30, 35, 40, 45,
+     50, 55, 60, 65, 70, 75, 80, 85, 90)
+)
+calories = st.sidebar.selectbox(
+    'Select calories goal',
+    (50, 100, 150, 200,
+     250, 300, 350, 400,
+     450, 500, 550, 600
+     )
 )
 
 
@@ -52,86 +60,58 @@ st.write('Weekday:', weekday)
 
 st.write('Sport type:', sport_type)
 st.write('Duration:', duration)
+st.write('Calories goal:', calories)
 
 
-def get_dataset(name):
-    data = None
-    if name == 'Iris':
-        data = datasets.load_iris()
-    elif name == 'Wine':
-        data = datasets.load_wine()
-    else:
-        data = datasets.load_breast_cancer()
-    X = data.data
-    y = data.target
-    return X, y
+df = pd.read_csv("sport_data.csv")
+# drop location
+df = df.drop('Location (City)', axis=1)
+df = handle_categorical(df)
+duration_min = df["Duration (min)"].min()
+duration_max = df["Duration (min)"].max()
+calories_min = df["Calories Consumed"].min()
+calories_max = df["Calories Consumed"].max()
+df = merge_user_rows(df, duration_min, duration_max,
+                     calories_min,  calories_max)
+# print(df)
+
+# fit model
+X = df[list(df.columns)[1:]]
+# X["Duration (min)"] = (X["Duration (min)"] - X["Duration (min)"].min()
+#                        ) / (X["Duration (min)"].max() - X["Duration (min)"].min())
+# X["Calories Consumed"] = (X["Calories Consumed"] - X["Calories Consumed"].min()) / \
+#     (X["Calories Consumed"].max() - X["Calories Consumed"].min())
+neigh = NearestNeighbors(n_neighbors=2)
+nbrs = neigh.fit(X)
+
+query = [
+    [11, weekday, sport_type, duration, calories]
+]
+input_df = generate_input(query, df)
+processed_input = merge_user_rows(
+    input_df, duration_min, duration_max, calories_min,  calories_max).iloc[:, 1:].values.tolist()
+# processed_input["Duration (min)"] = (processed_input["Duration (min)"] - processed_input["Duration (min)"].min()
+#                                      ) / (processed_input["Duration (min)"].max() - processed_input["Duration (min)"].min())
+# processed_input["Calories Consumed"] = (processed_input["Calories Consumed"] - processed_input["Calories Consumed"].min()) / \
+#     (processed_input["Calories Consumed"].max() -
+#      processed_input["Calories Consumed"].min())
+print(f"processed_input: {processed_input}")
+
+distances, indices = nbrs.kneighbors(processed_input)
+print(f"indices: {indices}")
+usernames = []
+for index in indices[0]:
+    usernames.append(df.iloc[index, 0])
+print(f"usernames: {usernames}")
+
+output_users = ""
+for user in usernames:
+    output_users += user
+    output_users += ","
+st.write('Mates selected:', output_users)
+
+# for index in indices:
+#   print(df.iloc[index])
 
 
-# X, y = get_dataset(dataset_name)
-# st.write('Shape of dataset:', X.shape)
-# st.write('number of classes:', len(np.unique(y)))
-
-
-def add_parameter_ui(clf_name):
-    params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0)
-        params['C'] = C
-    elif clf_name == 'KNN':
-        K = st.sidebar.slider('K', 1, 15)
-        params['K'] = K
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        params['n_estimators'] = n_estimators
-    return params
-
-# params = add_parameter_ui(classifier_name)
-
-
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'SVM':
-        clf = SVC(C=params['C'])
-    elif clf_name == 'KNN':
-        clf = KNeighborsClassifier(n_neighbors=params['K'])
-    else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'],
-                                           max_depth=params['max_depth'], random_state=1234)
-    return clf
-
-# clf = get_classifier(classifier_name, params)
-#### CLASSIFICATION ####
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-
-# clf.fit(X_train, y_train)
-# y_pred = clf.predict(X_test)
-
-# acc = accuracy_score(y_test, y_pred)
-
-# st.write(f'Classifier = {classifier_name}')
-# st.write(f'Accuracy =', acc)
-
-#### PLOT DATASET ####
-# Project the data onto the 2 primary principal components
-# pca = PCA(2)
-# X_projected = pca.fit_transform(X)
-
-# x1 = X_projected[:, 0]
-# x2 = X_projected[:, 1]
-
-# fig = plt.figure()
-# plt.scatter(x1, x2,
-#         c=y, alpha=0.8,
-#         cmap='viridis')
-
-# plt.xlabel('Principal Component 1')
-# plt.ylabel('Principal Component 2')
-# plt.colorbar()
-
-
-# #plt.show()
-# st.pyplot(fig)
 print("it renders again")
